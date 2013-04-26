92a8fe6149e44a8f7609e3fc2be878088e3a93d6
Jorge  Angel Hernandez Ayala <angelkz55@gmail.com>
2013-04-26 10:43:50 -0700

    Agrege algo
    Aqui logre hacer.... y aham.

---
+++ Segundo  proyecto/Cuadro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class Cuadro
+    {
+        Point posicion;
+        int lado;
+        Pen contorno;
+        Color fondo;
+        public enum Estado { Estatico, Movimiento};
+        public enum Direccion {arrriba,abajo,derecha,izquierda}
+        Estado estado;
+        Direccion direccion;
+
+        public Cuadro(Point p, int l, Pen c, Color f, Estado e)
+        {
+            this.posicion = p; this.lado = l; this.contorno = c; this.fondo = f; this.estado = e;
+        }
+
+        public void Draw(PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(contorno, posicion.X - 10, posicion.Y - 10, lado, lado);
+        }
+
+
+
+       
+
+    }
+}
---
+++ Segundo  proyecto/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Point p=new Point (100,100);
+            Pen a=new Pen (Color.Red);
+
+            Cuadro c = new Cuadro(p, 20, a, Color.Green, Cuadro.Estado.Estatico);
+            c.Draw(e);
+            
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+    }
+}
